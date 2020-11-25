3fba87d483e25624197b52f79d53c90e3b567b11
Christopher <chris.tryber@gmail.com>
2020-11-25 17:53:16 -0300

    Exercicio 3

---
+++ exercises/bloco_4/dia_1/maiorDeTres.js
@@ -0,0 +1,13 @@
+let a = 5;
+let b = 10;
+let c = 20;
+
+if (a > b && a > c) {
+    console.log(a + ' é o maior número')
+}else if (b > a && b > c) {
+    console.log(b + ' é o maior número')
+}else{
+    console.log(c + ' é o maior número')
+}
+
+//Deu certo
