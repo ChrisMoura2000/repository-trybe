fd4bd954ddd5d7f9cff8f609788a2f36ad9c84ec
Christopher <chris.tryber@gmail.com>
2020-11-26 12:01:05 -0300

    Exercicio 8

---
+++ exercises/bloco_4/dia_1/verifacaPar.js
@@ -0,0 +1,11 @@
+let a = 1;
+let b = 2;
+let c = 3;
+
+if (a%2 > 0 || b%2 > 0 || c%2 > 0){
+    console.log('True - pelo menos um número é impar.')
+}else{
+    console.log('False - nenhum número é impar.')
+}
+
+//Deu certo
