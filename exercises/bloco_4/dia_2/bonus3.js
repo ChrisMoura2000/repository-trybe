fb9ae23a8ee42a0ff9d14f0ddd9cf7d0bb6ff42c
Christopher <chris.tryber@gmail.com>
2020-11-27 12:28:32 -0300

    Corrigi o bonus 3

--- exercises/bloco_4/dia_2/bonus3.js
+++ exercises/bloco_4/dia_2/bonus3.js
@@ -4,8 +4,14 @@ let newArray = [];
 for(let cont = 1; cont < numbers.length; cont += 1){
     let multi = numbers[cont] * numbers[cont - 1];
     newArray.push(multi);
+    if (numbers[cont + 1] === undefined){
+        multi = numbers[cont] * 2;
+        newArray.push(multi)
+    }
+  
 }
 
 console.log(newArray);
 
-//Deu super certo 
+//Deu super certo mas o ultimo valor ta errado!
+//Agora sim deu certo de verdade kkkkkkk!
