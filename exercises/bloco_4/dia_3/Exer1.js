dfd61aa52d9c31d15d1d2611db5bb3ba68ef517e
Christopher <chris.tryber@gmail.com>
2020-11-27 17:30:50 -0300

    Corrigi o Exercicio 1

--- exercises/bloco_4/dia_3/Exer1.js
+++ exercises/bloco_4/dia_3/Exer1.js
@@ -1,10 +1,10 @@
 let n = 5; 
 let asterico = '';
 
-for (let i = 0; i <= n; i += 1){
+for (let i = 1; i <= n; i += 1){
     asterico += '*'
 }
 
-for (let j = 0; j <= n; j += 1){
+for (let j = 1; j <= n; j += 1){
     console.log(asterico)
 }
