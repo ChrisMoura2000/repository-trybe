817317f9990443207a404795ee7486c025b4f645
Christopher <chris.tryber@gmail.com>
2021-01-04 19:13:45 -0300

    finalizando exercicio web storage

--- exercises/bloco_5/dia_4/script.js
+++ exercises/bloco_5/dia_4/script.js
@@ -16,3 +16,20 @@ colorTxt.addEventListener('change', function (){
 })
 document.body.style.color = localStorage.corFonte;
 
+fontSize.addEventListener('change', function(){
+    localStorage.setItem('tamanhoFont', fontSize.value);
+    document.body.style.fontSize = localStorage.tamanhoFont +'pt';
+})
+document.body.style.fontSize = localStorage.tamanhoFont +'pt';
+
+espaceLetters.addEventListener('change', function(){
+    localStorage.setItem('spaceLine', espaceLetters.value);
+    document.body.style.lineHeight = localStorage.spaceLine + 'px';
+});
+document.body.style.lineHeight = localStorage.spaceLine + 'px';
+
+fontStyle.addEventListener('change', function(){
+    localStorage.setItem('familyFont', fontStyle.value);
+    document.body.style.fontFamily = localStorage.familyFont;
+});
+document.body.style.fontFamily = localStorage.familyFont;
