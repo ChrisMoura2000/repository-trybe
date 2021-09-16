4044c3a677a4dd0945bf792a075b07809a79cd85
Christopher <chris.tryber@gmail.com>
2021-09-16 12:46:11 -0300

    exercise 5

--- Ciencia da Computacao/33_1/exercises.py
+++ Ciencia da Computacao/33_1/exercises.py
@@ -1,3 +1,5 @@
+import math
+
 # Ex1
 
 
@@ -31,7 +33,7 @@ def print_square(square):
             print(square * "*")
 
 
-# Ex3
+# Ex4
 
 
 def bigest_name(list_name):
@@ -40,4 +42,21 @@ def bigest_name(list_name):
         if result < name:
             result = name
 
+    return result
+
+
+# Ex5
+
+
+# 1 Litro cobre 3 metros quadrados
+# 1 Lata de tinta tem 18 litros e custa R$ 80,00
+# Quantas latas eu preciso comprar e quanto vai custar
+
+
+def calc_ink(space):
+    paint_cans = math.ceil((space / 3) / 18)
+    if paint_cans < 1:
+        paint_cans = 1
+    price = paint_cans * 80.00
+    result = (paint_cans, price)
     return result
