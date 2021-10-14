a7af80d8716f950040ed7c76105ee3ce90fb6d46
Christopher <chris.tryber@gmail.com>
2021-10-14 18:38:48 -0300

    start 34.3 new version

---
+++ Ciencia da Computacao/34_1/exercises.py
@@ -0,0 +1,39 @@
+# Exercício 1: Em um contexto de orientação a objetos, como podemos definir o que são mensagens e qual a sua importância?
+
+# Resposta 1:  Forma com que objetos interagem - chamando funções uns dos outros. Um chamado como esse é um envio de mensagem a outro objeto. "User, resete sua senha!"
+
+
+# Exercício 2: Para exercitar nossa capacidade de abstração, vamos modelar algumas partes de um software de geometria. Como poderíamos modelar um objeto retângulo?
+
+# Nome da abstração
+# Retangulo
+
+# atributos/estados
+# - base, altura (tamanho)
+
+# comportamentos
+# - calcular area (base * altura)
+# - calcular perímetro (2*base) + (2*altura)
+
+
+# Exercício 3: E como poderíamos definir um círculo? Qual seu nome, atributos e comportamentos?
+
+# Nome da abstração
+# Circulo
+
+# atributos/estados
+# - raio
+
+# comportamentos
+# - calcular area (PI * raio²)
+
+# Exercício 4: Vamos mudar um pouco nosso contexto para um sistema de vendas de uma cafeteria. Como podemos abstrair um pedido composto por vários itens? Qual seu nome, atributos e comportamentos?
+
+# Nome da abstração
+# Venda
+
+# atributos/estados
+# - itens, valores
+
+# comportamentos
+# - Calcula troco (total pago - total a pagar)
--- Ciencia da Computacao/34_2/exercicio2.py
+++ Ciencia da Computacao/34_2/exercicio2.py
@@ -2,17 +2,18 @@ from statistics import median, mean, mode
 
 
 class Estatistica:
-    def media(self, lista_medias):
-        print(mean(lista_medias))
+    def __init__(self, lista):
+        self.lista = lista
 
-    def mediana(self, lista_mediana):
-        print(median(lista_mediana))
+    def media(self):
+        print(mean(self.lista))
 
-    def moda(self, lista_moda):
-        print(mode(lista_moda))
+    def mediana(self):
+        print(median(self.lista))
+
+    def moda(self):
+        print(mode(self.lista_moda))
 
 
-estatistica = Estatistica()
-estatistica.media([5, 5, 5])
-estatistica.mediana([10, 15, 5])
-estatistica.moda([1, 2, 1, 5, 7, 4, 9, 8, 5, 6, 3])
+estatistica = Estatistica([1, 2, 1, 5, 7, 4, 9, 8, 5, 6, 3])
+estatistica.moda()
